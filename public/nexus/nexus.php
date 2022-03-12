6e003d67c1c5c4b1793f796386ef6a0d1588c3c1
Jitendra <jeet21192@hotmail.com>
2022-03-12 19:02:08 +0530

    env file updated in ag folder

--- public/nexus/nexus.php
+++ public/nexus/nexus.php
@@ -6,36 +6,39 @@ if (empty($eventid)) {
     echo "<center><h1>OOPS!<br/><br/>Invalid Event ID provided</h1></center>";
 }
 
-$url = 'http://194.233.65.10/LiveTV/TVApi.svc/GetLiveTV?eventid=' . $eventid;
+//$url = 'http://194.233.65.10/LiveTV/TVApi.svc/GetLiveTV?eventid=' . $eventid;
+//
+//$headers = array('Content-Type: application/json');
+//$process = curl_init();
+//curl_setopt($process, CURLOPT_URL, $url);
+//curl_setopt($process, CURLOPT_HTTPHEADER, $headers);
+//curl_setopt($process, CURLOPT_CUSTOMREQUEST, "GET");
+//curl_setopt($process, CURLOPT_TIMEOUT, 30);
+//curl_setopt($process, CURLOPT_HTTPGET, 1);
+//curl_setopt($process, CURLOPT_SSL_VERIFYPEER, false);
+//curl_setopt($process, CURLOPT_RETURNTRANSFER, TRUE);
+//$return = curl_exec($process);
+//curl_close($process);
+//
+//$match_data = json_decode($return, true);
+//
+////echo __FILE__." at line ".__LINE__."<br>";echo "<pre>";print_r($match_data);die();
+//
+//if(isset($match_data['result']) && count($match_data['result']) > 0 && isset($match_data['result'][0])) {
+//    if(isset($match_data['result'][0]['link']) && !empty($match_data['result'][0]['link'])) {
+//        $tvLink = $match_data['result'][0]['link'];
+//    }else if(isset($match_data['result'][0]['link2']) && !empty($match_data['result'][0]['link2'])) {
+//        $tvLink = $match_data['result'][0]['link2'];
+//    }
+//}
+//
+//if(!isset($tvLink)){
+//    echo "<center><h1>TV not available right now</h1></center>";die();
+//}
 
-$headers = array('Content-Type: application/json');
-$process = curl_init();
-curl_setopt($process, CURLOPT_URL, $url);
-curl_setopt($process, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($process, CURLOPT_CUSTOMREQUEST, "GET");
-curl_setopt($process, CURLOPT_TIMEOUT, 30);
-curl_setopt($process, CURLOPT_HTTPGET, 1);
-curl_setopt($process, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($process, CURLOPT_RETURNTRANSFER, TRUE);
-$return = curl_exec($process);
-curl_close($process);
+$tvLink = "https://orangeexch999.com/jmp/nm3/all.php?eventId=".$eventid;
 
-$match_data = json_decode($return, true);
-
-//echo __FILE__." at line ".__LINE__."<br>";echo "<pre>";print_r($match_data);die();
-
-if(isset($match_data['result']) && count($match_data['result']) > 0 && isset($match_data['result'][0])) {
-    if(isset($match_data['result'][0]['link']) && !empty($match_data['result'][0]['link'])) {
-        $tvLink = $match_data['result'][0]['link'];
-    }else if(isset($match_data['result'][0]['link2']) && !empty($match_data['result'][0]['link2'])) {
-        $tvLink = $match_data['result'][0]['link2'];
-    }
-}
-
-if(!isset($tvLink)){
-    echo "<center><h1>TV not available right now</h1></center>";die();
-}
-
+//die($tvLink);
 ?>
 <html>
 <head>
@@ -52,8 +55,9 @@ if(!isset($tvLink)){
     <script src="https://cdnjs.cloudflare.com/ajax/libs/video.js/7.0.0/video.min.js"></script>
 </head>
 <body style="margin: 0;width: 100%;height: 100%;display: block;position: relative;">
-<video id="my_video_1" class="video-js vjs-fluid vjs-default-skin" controls autoplay loop muted data-setup='{"fluid": true, "autoplay": true}' style="height: 270px;padding: 0;">
-    <source src="<?php echo $tvLink; ?>" type="application/x-mpegURL">
-</video>
+<iframe src="<?php echo $tvLink; ?>" frameborder="0" style="height: 100%;padding: 0;width: 100%"></iframe>
+<!--<video src= id="my_video_1" class="video-js vjs-fluid vjs-default-skin" controls autoplay loop muted data-setup='{"fluid": true, "autoplay": true}' style="height: 270px;padding: 0;">-->
+<!--    <source src="--><?php //echo $tvLink; ?><!--" type="application/x-mpegURL">-->
+<!--</video>-->
 </body>
 </html>
