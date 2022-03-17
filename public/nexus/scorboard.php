869a16d7019ca5d30fea83b093513dbe3b84860e
Jitendra <jeet21192@hotmail.com>
2022-03-17 10:05:26 +0530

    log table name issues

--- public/nexus/scorboard.php
+++ public/nexus/scorboard.php
@@ -3,25 +3,26 @@ error_reporting(0);
 $eventId = $_GET['event_id'];
 $match_type = $_GET['match_type'];
 
-if($match_type == 'cricket') {
-    $url = "http://marketsarket.in:3002/scoreurl/".$eventId;
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_URL, $url);
-    $res = curl_exec($ch);
-
-    $arr = json_decode($res,true);
-
-
-    if(isset($arr['score'])){
-        $explode = explode("/",$arr['score']);
-        $eventId2 = end($explode);
-    }else{
-        echo "<center><h1>OOPS!<br>Scoreboard not available right now</h1></center>";die();
-    }
-    $iFrameUrl = "https://central.satsport247.com/score_widget/" . $eventId2;
-    $height = '180px;';
-}else{
+//if($match_type == 'cricket') {
+//    $url = "http://marketsarket.in:3002/scoreurl/".$eventId;
+//    $ch = curl_init();
+//    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//    curl_setopt($ch, CURLOPT_URL, $url);
+//    $res = curl_exec($ch);
+//
+//    $arr = json_decode($res,true);
+//
+//
+//    if(isset($arr['score'])){
+//        $explode = explode("/",$arr['score']);
+//        $eventId2 = end($explode);
+//    }else{
+//        echo "<center><h1>OOPS!<br>Scoreboard not available right now</h1></center>";die();
+//    }
+//    $iFrameUrl = "https://central.satsport247.com/score_widget/" . $eventId2;
+//    $height = '180px;';
+//}else
+{
     $height = '110px;';
     $iFrameUrl = "https://bfscore.onlyscore.live/?id=" . $eventId;
 }
