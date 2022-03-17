8c08f039b61560a4ba814b533be51da3992944e6
Jitendra <jeet21192@hotmail.com>
2022-03-17 22:27:42 +0530

    tv changes

--- public/nexus/nexus.php
+++ public/nexus/nexus.php
@@ -59,9 +59,9 @@ if(!isset($tvLink)){
     <script src="https://cdnjs.cloudflare.com/ajax/libs/video.js/7.0.0/video.min.js"></script>
 </head>
 <body style="margin: 0;width: 100%;height: 100%;display: block;position: relative;">
-<!--<iframe src="--><?php //echo $tvLink; ?><!--" frameborder="0" style="height: 100%;padding: 0;width: 100%"></iframe>-->
-<video src= id="my_video_1" class="video-js vjs-fluid vjs-default-skin" controls autoplay loop muted data-setup='{"fluid": true, "autoplay": true}' style="height: 270px;padding: 0;">
-    <source src="<?php echo $tvLink; ?>" type="application/x-mpegURL">
-</video>
+<iframe src="<?php echo $tvLink; ?>" frameborder="0" style="height: 100%;padding: 0;width: 100%"></iframe>
+<!--<video src= id="my_video_1" class="video-js vjs-fluid vjs-default-skin" controls autoplay loop muted data-setup='{"fluid": true, "autoplay": true}' style="height: 270px;padding: 0;">-->
+<!--    <source src="--><?php //echo $tvLink; ?><!--" type="application/x-mpegURL">-->
+<!--</video>-->
 </body>
 </html>
