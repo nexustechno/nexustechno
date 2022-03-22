638d601f84627700960beb77a4b3c8c180a54b5b
Jitendra <jeet21192@hotmail.com>
2022-03-22 22:46:45 +0530

    api added

--- public/nexus/scorboard.php
+++ public/nexus/scorboard.php
@@ -2,18 +2,42 @@
 error_reporting(0);
 $eventId = $_GET['event_id'];
 $match_type = $_GET['match_type'];
-$height = '110px;';
-if($match_type == 'cricket') {
-    $matchdate = $_GET['date'];
-    $time = $_GET['time'];
-    $iFrameUrl = "https://www.shivexch.com/cricket_scoree/index.html?matchDate=".$matchdate." ".$time."&mtid=".$eventId;
+//$height = '110px;';
+//if($match_type == 'cricket') {
+//    $matchdate = $_GET['date'];
+//    $time = $_GET['time'];
+//    $iFrameUrl = "https://www.shivexch.com/cricket_scoree/index.html?matchDate=".$matchdate." ".$time."&mtid=".$eventId;
+//}
+//elseif($match_type == 'tennis'){
+//    $iFrameUrl = "https://shivexch.com/tennis_score3/index.html?eventid=" . $eventId;
+//}
+//elseif($match_type == 'soccer'){
+//    $iFrameUrl = "https://www.shivexch.com/soccer_scoree/index.html?eventid=" . $eventId;
+//}
+
+if($match_type == 'cricket' || $match_type == 'tennis') {
+    $url = "http://marketsarket.in:3002/scoreurl/".$eventId;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    $res = curl_exec($ch);
+
+    $arr = json_decode($res,true);
+
+
+    if(isset($arr['score'])){
+        $explode = explode("/",$arr['score']);
+        $eventId2 = end($explode);
+    }else{
+        echo "<center><h1>OOPS!<br>Scoreboard not available right now</h1></center>";die();
+    }
+    $iFrameUrl = "https://central.satsport247.com/score_widget/" . $eventId2;
+    $height = '180px;';
+}else{
+    $height = '110px;';
+    $iFrameUrl = "https://bfscore.onlyscore.live/?id=" . $eventId;
 }
-elseif($match_type == 'tennis'){
-    $iFrameUrl = "https://shivexch.com/tennis_score3/index.html?eventid=" . $eventId;
-}
-elseif($match_type == 'soccer'){
-    $iFrameUrl = "https://www.shivexch.com/soccer_scoree/index.html?eventid=" . $eventId;
-}
+
 ?>
 <html>
 <head>
