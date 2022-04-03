fea29b93e2f4d7f80f18f65d24a930f8bca5afe3
Jitendra <jeet21192@hotmail.com>
2022-04-03 23:26:57 +0530

    casino video solution

--- public/nexus/redirect.php
+++ public/nexus/redirect.php
@@ -2,7 +2,7 @@
 
 $game = $_GET['game'];
 
-$baseLink = 'http://64.227.160.16/player.php?game='.$game;
+$url = 'http://64.227.160.16/player.php?game='.$game;
 
 $homepage = file_get_contents($url);
 echo $homepage;
