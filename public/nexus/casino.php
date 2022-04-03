3153856e7d929be4a7ec1d1d04ef0495b7ef5c63
Jitendra <jeet21192@hotmail.com>
2022-04-03 23:26:26 +0530

    casino video solution

--- public/nexus/casino.php
+++ public/nexus/casino.php
@@ -13,7 +13,7 @@ $baseLink = 'http://64.227.160.16/player.php?game='.$game;
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 <body style="margin: 0;width: 100%;height: 100%;display: block;position: relative;margin: 0 auto;text-align: center;background: black;">
-    <iframe id="dnn_ctr560_ProductDetails_ctl02_iframe1" src="./redirect.php?url=<?php echo $baseLink; ?>" frameborder="0" style="padding: 0;"></iframe>
+    <iframe id="dnn_ctr560_ProductDetails_ctl02_iframe1" src="./redirect.php?game=<?php echo $game; ?>" frameborder="0" style="padding: 0;"></iframe>
 </body>
 <script type="text/javascript">
     //$(document).ready(function () {
--- public/nexus/redirect.php
+++ public/nexus/redirect.php
@@ -1,3 +1,8 @@
 <?php
+
+$game = $_GET['game'];
+
+$baseLink = 'http://64.227.160.16/player.php?game='.$game;
+
 $homepage = file_get_contents($url);
 echo $homepage;
